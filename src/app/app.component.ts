1323c9a8edf2d698fcb6233ff3278ab7ad7a8d8a
Caleb Miller <caleb@mathmammoth.com>
2020-10-16 21:57:52 -0600

    set title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private title: Title) {}
+
+  ngOnInit() {
+    this.title.setTitle('Chuck Norris Jokes Generator');
+  }
+}
